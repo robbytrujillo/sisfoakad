72a8f1fa1c639722016b5bebbc9eb5c3794d9f0d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-05-16 15:32:49 +0700

    Administrator Page

--- application/views/administrator/dashboard.php
+++ application/views/administrator/dashboard.php
@@ -7,6 +7,86 @@
         <h4 class="alert-heading">Selamat Datang!</h4>
         <p>Selamat Datang <strong><?php echo $username; ?></strong> di Sistem Informasi Akademik IHBS, Anda login sebagai <strong><?php echo $level; ?></strong></p>
         <hr>
-        <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+        <button type="button" class="btn btn-sm btn-info" data-toggle="modal" data-target="#exampleModal">
+        <i class="fas fa-cogs"></i> Control Panel
+        </button>
+    </div>
+
+    <!-- Button trigger modal -->
+    
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-cogs"></i> Control Panel</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <div class="row">
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">MAHASISWA</p></a>
+                    <i class="fas fa-3x fa-user-graduate"></i>
+                </div>
+                    <div class="col-md-3 text-info text-center">
+                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">TAHUN AKADEMIK</p></a>
+                    <i class="fas fa-3x fa-calendar-alt"></i> 
+                 </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">KRS</p></a>
+                    <i class="fas fa-3x fa-edit"></i>
+                </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">KHS</p></a>
+                    <i class="fas fa-3x fa-file-alt"></i>
+                </div>
+            </div> 
+            <hr>
+            <div class="row">
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">INPUT NILAI</p></a>
+                    <i class="fas fa-3x fa-sort-numeric-down"></i>
+                </div>
+                    <div class="col-md-3 text-info text-center">
+                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">CETAK TRANSKIP</p></a>
+                    <i class="fas fa-3x fa-print"></i> 
+                 </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">KATEGORI</p></a>
+                    <i class="fas fa-3x fa-list-ul"></i>
+                </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">INFO KAMPUS</p></a>
+                    <i class="fas fa-3x fa-bullhorn"></i>
+                </div>
+            </div> 
+            <hr>
+            <div class="row">
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">IDENTITAS</p></a>
+                    <i class="fas fa-3x fa-id-card-alt"></i>
+                </div>
+                    <div class="col-md-3 text-info text-center">
+                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">TENTANG KAMPUS</p></a>
+                    <i class="fas fa-3x fa-info-circle"></i> 
+                 </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">FASILITAS</p></a>
+                    <i class="fas fa-3x fa-laptop"></i>
+                </div>
+                <div class="col-md-3 text-info text-center">
+                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">GALERI</p></a>
+                    <i class="fas fa-3x fa-images"></i>
+                </div>
+            </div> 
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        </div>
+    </div>
     </div>
 </div>
