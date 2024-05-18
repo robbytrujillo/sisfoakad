aea011d69cb770424f3790bb59f603f6f090d321
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-05-18 10:28:24 +0700

    Prodi Page

--- application/controllers/administrator/prodi.php
+++ application/controllers/administrator/prodi.php
@@ -16,6 +16,27 @@ class Prodi extends CI_Controller {
         $this->load->view('administrator/prodi_form',$data);
         $this->load->view('templates_administrator/footer');
     }
+
+    public function tambah_prodi_aksi() {
+        $this->rules();
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->tambah_prodi();
+        } else {
+            $kode_prodi = $this->input->post('kode_prodi');
+            $nama_prodi = $this->input->post('nama_prodi');
+            $nama_jurusan = $this->input->post('nama_jurusan');
+
+            $data = array(
+                'kode_prodi' => $kode_prodi,
+                'nama_prodi' => $nama_prodi,
+                'nama_jurusan' => $nama_jurusan,
+            );
+
+            $this->prodi_model->insert_data($data, 'prodi');
+            redirect('administrator/prodi');
+        }
+    }
     
 }
 
