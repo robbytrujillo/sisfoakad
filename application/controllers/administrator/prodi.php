ada91d066a58a51f13a07d0bc54c13dd47e5fd10
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-05-20 08:42:54 +0700

    Prodi Page

--- application/controllers/administrator/prodi.php
+++ application/controllers/administrator/prodi.php
@@ -49,6 +49,45 @@ class Prodi extends CI_Controller {
         $this->form_validation->set_rules('nama_prodi', 'nama_prodi', 'required',['required' => 'Nama Prodi wajib diisi!']);
         $this->form_validation->set_rules('nama_jurusan', 'nama_jurusan', 'required',['required' => 'Nama Jurusan wajib diisi!']);
     }
+
+    public function update($id) {
+        $where = array('id_prodi' => $id);
+
+        $data['prodi'] = $this->db->query("select * from prodi prd, jurusan jrs where prd.id_jurusan=jrs.id_jurusan and prd.id_prodi='$id'")->result();
+        $data['jurusan'] = $this->prodi_model->tampil_data('jurusan')->result();
+
+        $this->load->view('templates_administrator/header');
+        $this->load->view('templates_administrator/sidebar');
+        $this->load->view('administrator/prodi_update',$data);
+        $this->load->view('templates_administrator/footer');
+        }
+        
+        public function update_aksi() {
+            $kode_prodi = $this->input->post('kode_prodi');
+            $nama_prodi = $this->input->post('nama_prodi');
+            $nama_jurusan = $this->input->post('nama_jurusan');
+
+            $data = array(
+                'id_prodi' => $id_prodi,
+                'nama_prodi' => $nama_prodi,
+                'nama_jurusan' => $nama_jurusan,
+            );
+
+            $where = array(
+                'id_prodi' => $id,
+            );
+
+            $this->prodi_model->update_data($where,$data,'prodi');
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                                                        Data prodi berhasil diubah!
+                                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                        </div>');
+            redirect('administrator/peodi');
+        }
+
+    }
     
 }
 
--- application/models/prodi_model.php
+++ application/models/prodi_model.php
@@ -8,6 +8,11 @@ class Prodi_model extends CI_Model {
     public function insert_data($data, $table) {
         $this->db->insert($table, $data);
     }
+
+    public function update_data($where, $data, $table) {
+        $this->db->where($where);
+        $this->db->update($table, $data);
+    }
 }
 
 ?>
