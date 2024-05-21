2fbdec0a17ff13013102ece44c22dcc9f4f0d554
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-05-21 13:11:28 +0700

    Maahasiswa Page

--- application/views/administrator/mahasiswa.php
+++ application/views/administrator/mahasiswa.php
@@ -6,7 +6,23 @@
             <th>NAMA LENGKAP</th>
             <th>ALAMAT</th>
             <th>EMAIL</th>
-            <th class="colspan-3">AKSI</th>
+            <th colspan="3">AKSI</th>
         </tr>
+
+        <?php 
+        $no = 1;
+        foreach($mahasiswa as $mhs) : ?>
+
+            <tr>
+                <td><?php echo $no++ ?></td>
+                <td><?php echo $mhs->nama_lengkap  ?></td>
+                <td><?php echo $mhs->alamat  ?></td>
+                <td><?php echo $mhs->email  ?></td>
+                <td width="20px"><?php echo anchor('administrator/mahasiswa/detail/'.$mhs->id,'<div class="btn btn-sm btn-info"><i class="fa fa-eye"></i></div>') ?></td>
+                <td width="20px"><?php echo anchor('administrator/mahasiswa/update/'.$mhs->id,'<div class="btn btn-sm btn-primary"><i class="fa fa-edit"></i></div>') ?></td>
+                <td width="20px"><?php echo anchor('administrator/mahasiswa/delete/'.$mhs->id,'<div class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></div>') ?></td>
+            </tr>
+
+        <?php endforeach;?>
     </table>
 </div>
