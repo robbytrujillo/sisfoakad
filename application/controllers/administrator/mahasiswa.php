0e613d76de6b4d01eeafb21b27be8e89a6200ede
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-05-22 15:47:56 +0700

    Mahasiswa Page | update

--- application/controllers/administrator/mahasiswa.php
+++ application/controllers/administrator/mahasiswa.php
@@ -46,7 +46,7 @@ class Mahasiswa extends CI_Controller {
 
             } else {
                 $config['upload_path'] = './assets/uploads';
-                $config['allowed_types'] = 'jpg|png|gif\tiff';
+                $config['allowed_types'] = 'jpg|png|gif|tiff';
 
                 $this->load->library('upload',$config);
                 if (!$this->upload->do_upload('photo')) {
@@ -81,14 +81,7 @@ class Mahasiswa extends CI_Controller {
             }
         }
 
-        public functiion update($id) {
-            $where = array('id' => $id);
-            
-            $data['mahasiswa'] = $this->db->query("select * from mahasiswa mhs, prodi prd where mhs.nama_prodi = prd.nama_prodi and mhs.id = '$id'")->result();
-
-        };
-
-        public function _rules() {
+         public function _rules() {
             $this->form_validation->set_rules('nim','nim','required',['required' => 'NIM wajib diisi!']);
             $this->form_validation->set_rules('nama_lengkap','nama_lengkap','required',['required' => 'Nama Lengkap wajib diisi!']);
             $this->form_validation->set_rules('alamat','alamat','required',['required' => 'Alamat wajib diisi!']);
@@ -100,7 +93,77 @@ class Mahasiswa extends CI_Controller {
             $this->form_validation->set_rules('nama_prodi','nama_prodi','required',['required' => 'Nama Prodi wajib diisi!']);
             
         }
+
+        public function update($id) {
+            $where = array('id' => $id);
+            
+            $data['mahasiswa'] = $this->db->query("select * from mahasiswa mhs, prodi prd where mhs.nama_prodi = prd.nama_prodi and mhs.id = '$id'")->result();
+            $data['prodi'] = $this->matakuliah_model->tampil_data('prodi')->result();
+            $data['detail'] = $this->mahasiswa_model->ambil_id_mahasiswa($id);
+            $this->load->view('templates_administrator/header');
+            $this->load->view('templates_administrator/sidebar');
+            $this->load->view('administrator/mahasiswa_update',$data);
+            $this->load->view('templates_administrator/footer');
+        }
+
+        public function update_mahasiswa_aksi() {
+            $this->_rules();
+
+            if ($this->form_validation->run() == FALSE) {
+                $this->update();
+            } else {
+            $id             = $this->input->post('id');
+            $nim            = $this->input->post('nim');
+            $nama_lengkap   = $this->input->post('nama_lengkap');
+            $alamat         = $this->input->post('alamat');
+            $email          = $this->input->post('email');
+            $telepon        = $this->input->post('telepon');
+            $tempat_lahir   = $this->input->post('tempat_lahir');
+            $tanggal_lahir  = $this->input->post('tanggal_lahir');
+            $jenis_kelamin  = $this->input->post('jenis_kelamin');
+            $nama_prodi     = $this->input->post('nama_prodi');
+            $photo          = $_FILES['userfile']['name'];
+            
+            if ($photo ) {
+                $config['upload_path'] = './assets/uploads';
+                $config['allowed_types'] = 'jpg|png|gif|tiff';
+
+                $this->load->library('upload',$config);
+
+                if ($this->upload->do_upload('userfile')) {
+                    $userfile = $this->upload->data('file_name');
+                    $this->db->set('photo', $userfile);
+                } else {
+                    echo "Gagal Upload"; die();
+                }
+            }
+
+            $data = array(
+                'nim'                => $nim,
+                'nama_lengkap'       => $nama_lengkap,
+                'alamat'             => $alamat,
+                'email'              => $email,
+                'telepon'             => $telepon,
+                'tempat_lahir'       => $tempat_lahir,  
+                'tanggal_lahir'      => $tanggal_lahir,
+                'jenis_kelamin'      => $jenis_kelamin,
+                'nama_prodi'         => $nama_prodi,        
+            );
+
+            $where = array(
+                'id' => $id
+            );
+
+            $this->mahasiswa_model->update_data($where, $data, 'mahasiswa');
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                                                        Data Mahasiswa berhasil diubah!
+                                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                        </div>');
+            redirect('administrator/mahasiswa');
+            }
+        }
+
     }
-
-
 ?>
--- application/models/mahasiswa_model.php
+++ application/models/mahasiswa_model.php
@@ -18,6 +18,11 @@ class Mahasiswa_model extends CI_Model {
     public function insert_data($data,$table) {
         $this->db->insert($table,$data);    
     }
+
+    public function update_data($where, $data, $table) {
+        $this->db->where($where);
+        $this->db->update($table,$data);
+    }
 }
 
 ?>
