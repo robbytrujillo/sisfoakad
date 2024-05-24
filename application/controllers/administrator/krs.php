6595092fe0318121c5e60868371ed9cd317eef9e
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-05-24 15:14:52 +0700

    KRS page

--- application/controllers/administrator/krs.php
+++ application/controllers/administrator/krs.php
@@ -12,5 +12,27 @@ class Krs extends CI_Controller {
         $this->load->view('administrator/masuk_krs',$data);
         $this->load->view('templates_administrator/footer');
     }
+
+    public function krs_aksi() {
+        $this->_rulesKrs();
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->index();
+        } else {
+            $nim = $this->input->post('nim', TRUE);
+            $thn_akademik = $this->input->post('id_tahun_akademik', TRUE);
+
+        } 
+        
+        if ($this->mahasiswa_model->get_by_id($nim) == null) {
+             $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                                                    Data Mahasiswa Yang Anda Input belum terdaftar!
+                                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                    </div>');
+            redirect('administrator/krs');
+        }
+    }
 }
 ?>
