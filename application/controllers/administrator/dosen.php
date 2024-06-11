b6ccd4a8185bff9b8b9e38398e230a85f5e969a6
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-06-11 10:28:59 +0700

    Dosen Page

--- application/controllers/administrator/dosen.php
+++ application/controllers/administrator/dosen.php
@@ -21,7 +21,7 @@ class Dosen extends CI_Controller {
         // $data['prodi'] = $this->dosen_model->tampil_data('prodi')->result();
         $this->load->view('templates_administrator/header');
         $this->load->view('templates_administrator/sidebar');
-        $this->load->view('administrator/dosen_form',$data);
+        $this->load->view('administrator/dosen_form');
         $this->load->view('templates_administrator/footer');
     }
 
@@ -76,46 +76,37 @@ class Dosen extends CI_Controller {
         }
 
          public function _rules() {
-            $this->form_validation->set_rules('nim','nim','required',['required' => 'NIM wajib diisi!']);
-            $this->form_validation->set_rules('nama_lengkap','nama_lengkap','required',['required' => 'Nama Lengkap wajib diisi!']);
+            $this->form_validation->set_rules('nidn','nidn','required',['required' => 'NIDN wajib diisi!']);
+            $this->form_validation->set_rules('nama_dosen','nama_dosen','required',['required' => 'Nama Dosen wajib diisi!']);
             $this->form_validation->set_rules('alamat','alamat','required',['required' => 'Alamat wajib diisi!']);
+            $this->form_validation->set_rules('jenis_kelamin','jenis_kelamin','required',['required' => 'Jenis Kelamin wajib diisi!']);
             $this->form_validation->set_rules('email','email','required',['required' => 'Email wajib diisi!']);
             $this->form_validation->set_rules('telepon','telepon','required',['required' => 'Telepon wajib diisi!']);
-            $this->form_validation->set_rules('tempat_lahir','tempat_lahir','required',['required' => 'Tempat Lahir wajib diisi!']);
-            $this->form_validation->set_rules('tanggal_lahir','tanggal_lahir','required',['required' => 'Tanggal Lahir wajib diisi!']);
-            $this->form_validation->set_rules('jenis_kelamin','jenis_kelamin','required',['required' => 'Jenis Kelamin wajib diisi!']);
-            $this->form_validation->set_rules('nama_prodi','nama_prodi','required',['required' => 'Nama Prodi wajib diisi!']);
-            
         }
 
         public function update($id) {
-            $where = array('id' => $id);
-            
-            $data['mahasiswa'] = $this->db->query("select * from mahasiswa mhs, prodi prd where mhs.nama_prodi = prd.nama_prodi and mhs.id = '$id'")->result();
-            $data['prodi'] = $this->matakuliah_model->tampil_data('prodi')->result();
-            $data['detail'] = $this->mahasiswa_model->ambil_id_mahasiswa($id);
+            $where = array('nidn' => $id);
+            $data['dosen'] = $this->dosen_model->edit_data($where, 'dosen')->result();
+            $data['detail'] = $this->dosen_model->ambil_id_dosen($id);
             $this->load->view('templates_administrator/header');
             $this->load->view('templates_administrator/sidebar');
-            $this->load->view('administrator/mahasiswa_update',$data);
+            $this->load->view('administrator/dosen_update',$data);
             $this->load->view('templates_administrator/footer');
         }
 
-        public function update_mahasiswa_aksi() {
+        public function update_dosen_aksi() {
             $this->_rules();
 
             if ($this->form_validation->run() == FALSE) {
                 $this->update();
             } else {
-            $id             = $this->input->post('id');
-            $nim            = $this->input->post('nim');
-            $nama_lengkap   = $this->input->post('nama_lengkap');
+            $id             = $this->input->post('id_dosen');
+            $nidn           = $this->input->post('nidn');
+            $nama_dosen     = $this->input->post('nama_dosen');
             $alamat         = $this->input->post('alamat');
+            $jenis_kelamin  = $this->input->post('jenis_kelamin');
             $email          = $this->input->post('email');
             $telepon        = $this->input->post('telepon');
-            $tempat_lahir   = $this->input->post('tempat_lahir');
-            $tanggal_lahir  = $this->input->post('tanggal_lahir');
-            $jenis_kelamin  = $this->input->post('jenis_kelamin');
-            $nama_prodi     = $this->input->post('nama_prodi');
             $photo          = $_FILES['userfile']['name'];
             
             if ($photo ) {
@@ -133,42 +124,39 @@ class Dosen extends CI_Controller {
             }
 
             $data = array(
-                'nim'                => $nim,
-                'nama_lengkap'       => $nama_lengkap,
-                'alamat'             => $alamat,
-                'email'              => $email,
-                'telepon'             => $telepon,
-                'tempat_lahir'       => $tempat_lahir,  
-                'tanggal_lahir'      => $tanggal_lahir,
-                'jenis_kelamin'      => $jenis_kelamin,
-                'nama_prodi'         => $nama_prodi,        
+                'nidn'             => $nidn,
+                'nama_dosen'       => $nama_dosen,
+                'alamat'           => $alamat,
+                'jenis_kelamin'    => $jenis_kelamin,
+                'email'            => $email,
+                'telepon'          => $telepon,        
             );
 
             $where = array(
-                'id' => $id
+                'id_dosen' => $id
             );
 
-            $this->mahasiswa_model->update_data($where, $data, 'mahasiswa');
+            $this->dosen_model->update_data($where, $data, 'dosen');
             $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
-                                                        Data Mahasiswa berhasil diubah!
+                                                        Data Dosen berhasil diubah!
                                                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                                             <span aria-hidden="true">&times;</span>
                                                         </button>
                                                         </div>');
-            redirect('administrator/mahasiswa');
+            redirect('administrator/dosen');
             }
         }
 
         public function delete($id) {
-            $where = array('id' => $id);
-            $this->mahasiswa_model->hapus_data($where, 'mahasiswa');
+            $where = array('nidn' => $id);
+            $this->dosen_model->hapus_data($where, 'dosen');
             $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                                        Data Mahasiswa berhasil dihapus!
+                                                        Data Dosen berhasil dihapus!
                                                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                                             <span aria-hidden="true">&times;</span>
                                                         </button>
                                                         </div>');
-        redirect('administrator/mahasiswa');
+        redirect('administrator/dosen');
          }
     }
 ?>
--- application/models/dosen_model.php
+++ application/models/dosen_model.php
@@ -22,6 +22,10 @@ class Dosen_model extends CI_Model{
         }
     }
 
+    public function edit_data($where, $table) {
+        return $this->db->get_where($table,$where);
+    }
+
     public function update_data($where, $data, $table) {
         $this->db->where($where);
         $this->db->update($table,$data);
@@ -31,8 +35,6 @@ class Dosen_model extends CI_Model{
         $this->db->where($where);
         $this->db->delete($table);
     }
-
-    
 
     public function get_by_id($id) {
         $this->db->where($this->id,$id);
