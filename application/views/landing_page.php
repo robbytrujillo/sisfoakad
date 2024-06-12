4cc20c448b624992a9f7053ffe6821ae329c7b52
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2024-06-12 16:16:14 +0700

    Identity Page

--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -1,13 +1,14 @@
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-light bg-info">
 
   <?php foreach($identitas as $id) : ?>
   <a class="navbar-brand"><strong><?php echo $id->judul_website ?></strong></a>
-  <span class="small"><?php echo $id->alamat ?> - <?php echo $id->email ?> - <?php echo $id->telepon ?></span>
+  <span class="small" style="color: black"><?php echo $id->alamat ?> - <?php echo $id->email ?> - <?php echo $id->telepon ?></span>
   <?php endforeach; ?>
 
   <form class="form-inline">
     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
   </form>
 </nav>
 
@@ -17,12 +18,39 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav mx-auto">
-      <a class="nav-item nav-link" href="#">BERANDA <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">TENTANG KAMPUS</a>
-      <a class="nav-item nav-link" href="#">INFORMASI</a>
-      <a class="nav-item nav-link" href="#">FASILITAS</a>
-      <a class="nav-item nav-link" href="#">GALERI</a>
-      <a class="nav-item nav-link" href="#">KONTAK</a>
+      <a class="nav-item nav-link ml-3" href="#">BERANDA <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link ml-3" href="#">TENTANG KAMPUS</a>
+      <a class="nav-item nav-link ml-3" href="#">INFORMASI</a>
+      <a class="nav-item nav-link ml-3" href="#">FASILITAS</a>
+      <a class="nav-item nav-link ml-3" href="#">GALERI</a>
+      <a class="nav-item nav-link ml-3" href="#">KONTAK</a>
     </div>
   </div>
 </nav>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="<?php echo base_url('assets/img/slider1.jpg') ?>" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo base_url('assets/img/slider2.jpg') ?>" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo base_url('assets/img/slider3.jpg') ?>" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
